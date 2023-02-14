836ae5f5d33822da1b0b8af9711c601c574e7226
RodrigoRocS <rrsilva1701@gmail.com>
2023-02-14 17:46:06 -0300

    requisito 4 e 5 compl

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { createProductElement, createCustomElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productList = await fetchProductsList('computador');
 const secProduct = document.querySelector('.products');
+const loadings = document.getElementsByClassName('loading');
+const magicNumber = 1000;
 
-productList.forEach((e) => secProduct.appendChild(createProductElement(e)));
+const addProd = () => productList.forEach((e) => secProduct
+  .appendChild(createProductElement(e)));
+
+const loadingMsg = (time) => {
+  secProduct.appendChild(createCustomElement('span', 'loading', 'carregando...'));
+  setTimeout(async () => {
+    if (loadings.length) loadings[0].remove();
+    await addProd();
+  }, time);
+};
+
+loadingMsg(magicNumber);
