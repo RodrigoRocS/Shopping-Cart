696ce1c2c0913591265fc70f5c2a95d23b246d35
RodrigoRocS <rrsilva1701@gmail.com>
2023-02-14 15:23:12 -0300

    implementa func fetch

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,8 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (item) => {
+  const productList = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+  const productData = await productList.json();
+  return productData;
 };
