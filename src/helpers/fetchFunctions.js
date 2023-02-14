10dca5ac3b07bfd01cac7cea6a1831e80d169f10
RodrigoRocS <rrsilva1701@gmail.com>
2023-02-14 16:20:25 -0300

    requisito 2 compl correcao

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,9 +2,9 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async (item) => {
-  if (!item) throw new Error('Termo de busca não informado');
-  const productList = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${item}`);
+export const fetchProductsList = async (QUERY) => {
+  if (!QUERY) throw new Error('Termo de busca não informado');
+  const productList = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
   const data = await productList.json();
   return data.results;
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productList = await fetchProductsList('computador');
+
+createProductElement(productList);
